8849d9cc662698e0080713dd8cc992d1222ec466
dgallmeier@gmail.com <davidg@lyonesse.lan>
2015-12-12 01:52:12 -0500

    working on updatefloorplans
    ready to start on view after floorplans updater is working; need to
    read up on timezones and configure correctly

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,13 +15,10 @@ class JobsController < ApplicationController
       :camdencreekstone => 'https://www.camdenliving.com/atlanta-ga-apartments/camden-creekstone/apartments?bedrooms[]=12&bedrooms[]=9',
       :camdendunwoody => 'https://www.camdenliving.com/dunwoody-ga-apartments/camden-dunwoody/apartments?bedrooms[]=12&bedrooms[]=9&bedrooms[]=3'}
 
-    #mechanize is old
-    #mechanize = Mechanize.new
     #capybara/poultergeist/phantomjs is a headless javascript enabled browser!!!
     session = Capybara::Session.new(:poltergeist)
 
     sites.each_pair do |loc,url|
-      #page = mechanize.get(url)
       session.visit(url)
       page = session.html
 
@@ -29,8 +26,6 @@ class JobsController < ApplicationController
         location: loc, 
         url: url, 
         html: Base64.encode64(Zlib::Deflate.deflate(page.gsub("\u0000", ''))))
-
-      #sleep rand(45...75)
     end
 
     render :inline => "done"
@@ -44,27 +39,27 @@ class JobsController < ApplicationController
     unparsed_pagepulls = PagePull.find_by_sql(unparsed_pagepull_sql)
 
     div_classes = 'card-switch card-region-overlay pos-top pos-left pos-bottom inverted face-back'
+    new_listings = unparsed_pagepulls.collect {|upp|
+      Nokogiri::HTML(upp.dhtml).css("div[class='#{div_classes}']").
+      collect {|div|
+        div.css('tr').
+          select {|tr| tr.css('th').count == 0 }.
+          collect {|tr|
+            ApartmentListing.new do |a|
+              a.page_pull_id = upp.id
+              a.unitname = div.css('h3').css('span')[0].text
+              a.unitnum = tr.css('td')[0].text
+              a.floor = tr.css('td')[1].text
+              a.rent = tr.css('td')[2].text.gsub(/\$/,'')
+              a.movein = tr.css('td')[3].text
+            end
+          }.flatten(1)#end of div.collect
+      }.flatten(1)#end of Nokogiri.collect
+    }.flatten(1)#end of unparsed_pagepulls.collect
+
     ActiveRecord::Base.transaction do
-      new_listings = unparsed_pagepulls.collect {|upp|
-        Nokogiri::HTML(PagePull.first.dhtml).css('div').
-        select {|div| not div.attributes.nil? }.
-        select {|div| not div.attributes['class'].nil? }.
-        select {|div| div.attributes['class'].value.eql? div_classes }.
-        collect {|div|
-          div.css('tr').
-            select {|tr| tr.css('th').count == 0 }.
-            collect {|tr|
-              ApartmentListing.create(
-                page_pull_id: upp.id,
-                unitname: div.css('h3').css('span')[0].text,
-                unitnum: tr.css('td')[0].text,
-                floor: tr.css('td')[1].text,
-                rent: tr.css('td')[2].text.gsub(/\$/,''),
-                movein: tr.css('td')[3].text)
-            }.flatten(1)#end of div.collect
-        }.flatten(1)#end of Nokogiri.collect
-      }.flatten(1)#end of unparsed_pagepulls.collect
-    end#end of ActiveRecord::Basee.transaction
+      new_listings.each do |l| l.save end
+    end
 
     render :inline => "done"
   end
@@ -80,28 +75,26 @@ class JobsController < ApplicationController
       "from page_pulls " +
       "where created_at > (select latestdate from created_ats order by latestdate desc limit 1) " +
       "and location = 'camdendunwoody'"
-    pagepulls = PagePull.find_by_sql(unparsed_pagepulls_sql)
+    unparsed_pagepulls = PagePull.find_by_sql(unparsed_pagepulls_sql)
 
-    existing_floorplans = FloorPlan.all
-
-    current_floorplans = pagepulls.collect {|upp|
+    current_floorplans = unparsed_pagepulls.collect {|upp|
       Nokogiri::HTML(upp.dhtml).css("div[class='available-apartment-card default-gutter']").
         collect {|div|
           FloorPlan.new do |fp|
-            fp.name = div.css("div[class='panel-pane pane-entity-field pane-node-field-title-display inverted pos-left pos-bottom']").text.strip,
-            fp.sqft = div.css("div[class='card unit-info']").css('span')[1].text.gsub(/SqFt /,''),
-            fp.beds = div.css("div[class='card unit-info']").css('span')[2].text.gsub(/Beds /,''),
+            fp.name = div.css("h3 span[class='floorplan-name']").text
+            fp.sqft = div.css("div[class='card unit-info']").css('span')[1].text.gsub(/SqFt /,'')
+            fp.beds = div.css("div[class='card unit-info']").css('span')[2].text.gsub(/Beds /,'')
             fp.baths = div.css("div[class='card unit-info']").css('span')[3].text.gsub(/Baths /,'')
           end
         }
-    }.flatten(1)
+    }.flatten
 
-    #current_floorplans not in existing_floorplans
-    insert_floorplans = nil
-    #existing_floorplans in current_floorplans
-    update_floorplans = nil
-
-    binding.pry
+    ActiveRecord::Base.transaction do 
+      #insert new records
+      current_floorplans.each do |fp| fp.save end 
+      #update updated_at on floorplans (so that unprocessed page pulls will all be after latest updated_at)
+      #ActiveRecord::Base.connection.execute("update floor_plans set updated_at = current_time;")
+    end
 
     render :inline => "done"
   end
