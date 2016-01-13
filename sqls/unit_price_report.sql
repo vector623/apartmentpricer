1efffeed6214c1f6742fded2d75b5451949eb4b9
David G <davidg@fiame>
2016-01-13 18:14:28 -0500

    saving changes
    not sure how far along in fixing misranked unit price pages

--- sqls/unit_price_report.sql
+++ sqls/unit_price_report.sql
@@ -12,8 +12,22 @@
   movein,
   fetched_at,
   fetched_at::timestamp::date fetched_on,	
-  rank() over (partition by trust,location,unitname,unitnum order by fetched_at::timestamp::date) bdaterank,
-  rank() over (partition by trust,location,unitname,unitnum order by fetched_at::timestamp::date desc) edaterank
+  rank() over 
+    (partition by 
+      trust,
+      location,
+      unitname,
+      unitnum 
+    order by 
+      fetched_at::timestamp::date) bdaterank,
+  rank() over 
+    (partition by 
+      trust,
+      location,
+      unitname,
+      unitnum 
+    order by 
+      fetched_at::timestamp::date desc) edaterank
 from price_report),
 
 begindates as (select * from pricereport where bdaterank = 1),
@@ -21,8 +35,17 @@ begindates as (select * from pricereport where bdaterank = 1),
 enddates as (select * from pricereport where edaterank = 1),
 
 dates as
-(select i::date date
-from generate_series((select fetched_on from pricereport order by fetched_on limit 1),(select fetched_on from pricereport order by fetched_on desc limit 1),'1 day'::interval) i),
+(select 
+  i::date date
+from generate_series
+  ((select fetched_on 
+  from pricereport 
+  order by fetched_on limit 1),
+  (select fetched_on 
+  from pricereport 
+  order by fetched_on 
+  desc limit 1),
+  '1 day'::interval) i),
 
 pricereport_datedelimited as
 (select
@@ -33,9 +56,23 @@ pricereport_datedelimited as
   b.fetched_on as begindate,
   e.fetched_on as enddate
 from pricereport p
-join begindates b on p.trust = b.trust and p.location = b.location and p.unitname = b.unitname and p.unitnum = b.unitnum
-join enddates e on p.trust = e.trust and p.location = b.location and p.unitname = e.unitname and p.unitnum = e.unitnum
-group by p.trust,p.location,p.unitname,p.unitnum,b.fetched_on,e.fetched_on),
+join begindates b 
+  on p.trust = b.trust 
+  and p.location = b.location
+  and p.unitname = b.unitname 
+  and p.unitnum = b.unitnum
+join enddates e 
+  on p.trust = e.trust
+  and p.location = b.location
+  and p.unitname = e.unitname
+  and p.unitnum = e.unitnum
+group by 
+  p.trust,
+  p.location,
+  p.unitname,
+  p.unitnum,
+  b.fetched_on,
+  e.fetched_on),
 
 idx as
 (select 
@@ -57,16 +94,24 @@ idx_ranked_joined as
   i.date,
   p.rent,
   p.movein,
-  rank() over (partition by i.trust, i.location, i.unitname, i.unitnum, i.date order by p.fetched_at desc) daterank,
+  rank() over 
+    (partition by 
+      i.trust,
+      i.location,
+      i.unitname,
+      i.unitnum,
+      i.date
+    order by 
+      p.fetched_at) daterank,
   p.fetched_at,
   i.date = p.fetched_on priceupdated
 from idx i
 left outer join pricereport p 
-on i.trust = p.trust
-and i.location = p.location
-and i.unitname = p.unitname
-and i.unitnum = p.unitnum
-and i.date >= p.fetched_on
+  on i.trust = p.trust
+  and i.location = p.location
+  and i.unitname = p.unitname
+  and i.unitnum = p.unitnum
+  and i.date >= p.fetched_on
 order by i.trust,i.location,i.unitname,i.unitnum,i.date)
 
 select
